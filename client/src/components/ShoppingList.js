9261bc2c383093609d47f44030329912c078bd2a
dell <jmsugay11@gmail.com>
2020-04-21 18:35:22 +0800

    done

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -7,6 +7,13 @@ import PropTypes from 'prop-types';
 
  
 class ShoppingList extends Component {
+    static propTypes = {
+        getItems: PropTypes.func.isRequired,
+        item: PropTypes.object.isRequired,
+        isAuthenticated: PropTypes.bool
+    
+    }
+
     componentDidMount(){
         this.props.getItems();
     }
@@ -24,8 +31,12 @@ class ShoppingList extends Component {
                         {items.map(({ _id,name}) =>(
                             <CSSTransition key={ _id} timeout={500} classNames="fade">
                                 <ListGroupItem>
+                                    {
+                                    this.props.isAuthenticated ?
                                     <Button className="remove-btn" color="danger" size="sm" 
                                     onClick={this.onDeleteClick.bind(this, _id)}>&times;</Button>
+                                    : ""    
+                                    }
                                     {name}
                                 </ListGroupItem>
                             </CSSTransition>
@@ -37,14 +48,11 @@ class ShoppingList extends Component {
     }
 }
 
-ShoppingList.propTypes = {
-    getItems: PropTypes.func.isRequired,
-    item: PropTypes.object.isRequired
 
-}
 
 const mapStateToProps = (state) => ({
-    item: state.item
+    item: state.item,
+    isAuthenticated: state.auth.isAuthenticated
 })
 
 export default connect(mapStateToProps,{ getItems,deleteItem })(ShoppingList);
--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://node_test:santos31193@cluster0-z71wg.mongodb.net/test?retryWrites=true&w=majority'
-}
